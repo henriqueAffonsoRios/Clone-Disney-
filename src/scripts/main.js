004a0c33217134673eb15309c86b9021ef38c5ce
henriqueAffonsoRios <henriqueaffonsorios@gmail.com>
2024-03-20 14:19:50 -0300

    add faq, footer, header e responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   const buttons = document.querySelectorAll('[data-tab-button]');
+  const questions = document.querySelectorAll('[data-faq-question]');
+
+  const heroSection = document.querySelector('.hero');
+  const alturaHero = heroSection.clientHeight;
+
+  window.addEventListener('scroll', function(){
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero){
+      exibeElementosHeader();
+    }else{
+      ocultaElementosHeader();
+    }
+  })
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(btn) {
@@ -12,6 +26,29 @@ document.addEventListener("DOMContentLoaded", function() {
       removeFocusButton();
       btn.target.classList.add('shows__tabs__button--is-active');
     })
+  }
+
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", toggleAnswer)
+  }
+
+  function toggleAnswer(element) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = element.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+  }
+
+  function exibeElementosHeader(){
+    const header = document.querySelector('header');
+
+    header.classList.add('header--is-hidden')
+  }
+
+  function ocultaElementosHeader(){
+    const header = document.querySelector('header');
+
+    header.classList.remove('header--is-hidden')
   }
 
   function removeFocusButton() {
